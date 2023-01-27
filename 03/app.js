9202ba2255a2f5444270299918c4bd9ffcee2837
AlanVenegasT <alan4444v@gmail.com>
2023-01-27 00:24:08 -0600

    ya jala22222

--- 03/app.js
+++ 03/app.js
@@ -1,10 +1,41 @@
 
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs')
+            .option('b', {
+                alias: 'base',
+                type: 'number',
+                demandOption: true
+            })
+            .option('l', {
+                alias: 'listar',
+                type: 'boolean',
+                demandOption: true,
+                default: false
+            })
+            .check( (argv, options) => {
+                if( isNaN( argv.b)){
+                    throw 'La base tiene que ser un número'
+                }
+                return true;
+            })
+                .argv;
+
+
+
 
 console.clear();
 
-const base = 5;
+//console.log( process.argv );
+console.log( argv );
 
-crearArchivo( base )
+//console.log('base: yargs', argv.b)
+//const [,,arg3 = 'base=5'] = process.argv;
+//const [, base ] = arg3.split('=');
+//console.log( base );
+
+
+//const base = 5;
+
+crearArchivo( argv.b, argv.l )
     .then( nombreArchivo => console.log(nombreArchivo, 'creado'))
     .catch( err => console.log(err) );
